18d03a28590aea7e7d73401acdccbbaebb38b858
Michal Kubek <michal@numeus.xyz>
2024-01-08 05:22:04 +0000

    Change schema

--- init.lua
+++ init.lua
@@ -42,6 +42,11 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.g.python_flake8_options = '~/.config/nvim/setup.cfg'  -- Specify the path to your flake8 configuration file
+
+vim.g.yapf_options = '--style="{based_on_style: pep8, column_limit: 120}"'
+vim.g.black_line_length = 120
+vim.g.python_flake8_options = '--max-line-length=120'
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
@@ -69,8 +74,6 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
@@ -134,22 +137,21 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
     },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+--  { -- Add indentation guides even on blank lines
+--    'lukas-reineke/indent-blankline.nvim',
+--    -- Enable `lukas-reineke/indent-blankline.nvim`
+--    -- See `:help indent_blankline.txt`
+--    opts = {
+--      char = '┊',
+--      show_trailing_blankline_indent = false,
+--    },
+--  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
@@ -309,7 +311,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript',  'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -877,3 +879,10 @@ map("n",
     "Move right window")
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+
+vim.g.python_flake8_options = '~/.config/nvim/setup.cfg'  -- Specify the path to your flake8 configuration file
+
+vim.g.yapf_options = '--style="{based_on_style: pep8, column_limit: 120}"'
+vim.g.black_line_length = 120
+vim.g.python_flake8_options = '--max-line-length=120'
